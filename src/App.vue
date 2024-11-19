6181a26ea5271cfac4d69eea9cc0c4513d5c5b8a
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2024-11-18 21:46:20 -0800

    some updates are done

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
 import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
 import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import MenuItem from './components/MenuItem.vue';
 
 let openMenu = ref(false)
 </script>
@@ -30,9 +31,50 @@ let openMenu = ref(false)
       <button type="button" class="rounded-full bg-black p-[1px] ml-4 cursor-pointer">
         <ChevronRight fillColor="#FFFFFF" :size="30" />
       </button>
-
     </div>
 
+    <button 
+      @click="openMenu = !openMenu" :class="openMenu ? 'bg-[#282828]' : 'bg-black'"
+      class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer">
+      <div class="flex items-center">
+        <img src="https://lh3.googleusercontent.com/a/ACg8ocKFmO8hWfq2HAl1OnGjDff3VYXGViCM73xi_GFy923qtGcZfANxDQ=s519-c-no" class="rounded-full" width="27">
+        <div class="text-white text-[14px] ml-1.5 mr-1.5 font-semibold">
+          Sandesh
+        </div>
+        <ChevronDown v-if="!openMenu" @click="openMenu = true" fillColor="#FFFFFF" :size="25" />
+        <ChevronUp v-else @click="openMenu = false" fillColor="#FFFFFF" :size="25" />
+      </div>
+    </button>
+
+    <span
+      v-if="openMenu"
+      class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer"
+    >
+      <ul class="text-gray-200 font-semibold text-[14px]">
+        <li class="px-3 py-2.5 hover:bg-[#3E3D3D] border-b border-b-gray-600">
+          Profile
+        </li>
+        <li class="px-3 py-2.5 hover:bg-[#3E3D3D]">
+          Log Out
+        </li>
+      </ul>
+    </span>
+    </div>
+
+    <div
+    id="SideNav"
+    class="h-[100%] p-6 w-[240px] bg-black fixed z-50"
+    >
+      <router-link to="/">
+        <img width="125" src="/public/images/icons/spotify-logo.png">
+      </router-link>
+      <div class="my-8">
+        <ul>
+          <router-link >
+            <MenuItem/>
+          </router-link>
+        </ul>
+      </div>
     </div>
 
   </div>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref, toRefs, watchEffect} from 'vue';
+import {useRoute} from 'vue-router';
+const route = useRoute()
+
+const props = defineProps({
+    iconString: String,
+    iconSize: Number,
+    pageUrl: String,
+    name: String,
+})
+
+const {iconString, pageUrl, name, iconSize} = toRefs(props)
+
+let icon = ref(null)
+let textIsHover = ref(false)
+
+
+</script>
+
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
